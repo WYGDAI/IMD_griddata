4821039403b9e81a9065f028c5de243c1b525eb1
k.r.bro_05 <wygdai1905@gmail.com>
2025-02-10 22:26:35 +0530

    4_rename.py created and working
    Renaming an entire folder containing .nc files of a particular variable is possible

--- 4_rename.py
+++ 4_rename.py
@@ -5,8 +5,36 @@ This code will help in organizing the folder which contains the data that are re
     - a variety of datasets with varying names will be renamed to a standardized form as:
                             "2016-rain-IMD" or "1981-temp_min-IMD"
 
-* The variables that need to be adjusted are given from lines *
-    +
+* The variables that need to be adjusted are given from lines 18 and 19 *
+    + folder in which the files are present: "input_folder"
+    + variable of the files: "var"
 
                                                 - k.r.bro_05
 """
+import os
+import re
+import time
+
+input_folder = r"C:\Users\koust\Desktop\PhD\IMD_grid\4_IMD_grid_main\temp_min"
+var = 'temp_min'
+
+for file in os.listdir(input_folder):
+    if file.endswith(".nc"):
+        # Extracting year
+        match = re.search(r"\d{4}", file)
+        if match:
+            year = int(match.group())
+        else:
+            print(f"{file} is not a valid file to rename")
+            break
+
+        old_name_path = os.path.join(input_folder, file)
+        new_name = f"{year}-{var}-IMD.nc"
+        new_name_path = os.path.join(input_folder, new_name)
+
+        # Only rename the files that have old names
+        if not os.path.exists(new_name_path):
+            os.rename(old_name_path, new_name_path)
+        while not os.path.exists(new_name_path):
+            print(f"Waiting for file to be renamed to: {new_name}")
+            time.sleep(1)
